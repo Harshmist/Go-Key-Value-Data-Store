28b65853f162ed55ad71bc445437ccdc4cba22d4
Harshmist <ddawsonfitness@gmail.com>
2021-07-12 17:35:54 +0100

    Updated

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"strconv"
 	"strings"
@@ -10,7 +9,7 @@ import (
 
 var dta = make(map[int]string, 100)
 
-func data(w http.ResponseWriter, r *http.Request) {
+func listData(w http.ResponseWriter, r *http.Request) {
 
 	for i := 1; i < len(dta)+1; i++ {
 		fmt.Fprintf(w, strconv.FormatInt(int64(i), 10)+": ")
@@ -47,23 +46,32 @@ func getData(w http.ResponseWriter, r *http.Request) {
 }
 
 func post(w http.ResponseWriter, r *http.Request) {
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Error reading request body",
-			http.StatusInternalServerError)
-	}
-	dta[len(dta)+1] = string(body)
+	parts := strings.Split(r.URL.String(), "/")
 
-	fmt.Fprint(w, "POST done")
+	if len(parts) != 3 {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	// Allowing POST to take multiple words seperated by underscores "_"
+	var value string
+	valArr := strings.Split(parts[2], "_")
+	if len(valArr) > 1 {
+		value = strings.Join(valArr, " ")
+	} else {
+		value = parts[2]
+	}
+	dta[len(dta)+1] = string(value)
 }
 
 func main() {
+	// Starter data for testing
 	dta[1] = "David"
 	dta[2] = "Ryan"
 	dta[3] = "Craig"
 	dta[4] = "Liam"
-	http.HandleFunc("/data", data)
-	http.HandleFunc("/data/", getData)
-	http.HandleFunc("/data/post/", post)
+
+	http.HandleFunc("/list", listData)
+	http.HandleFunc("/get/", getData)
+	http.HandleFunc("/post/", post)
 	http.ListenAndServe(":8080", nil)
 }
