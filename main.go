514021739becaefb3794e7e92cd5e3faab5cba0a
Harshmist <ddawsonfitness@gmail.com>
2021-07-23 12:59:14 +0100

    Channels Added

--- main.go
+++ main.go
@@ -11,14 +11,127 @@ import (
 	"strings"
 )
 
-var dta = make(map[int]string, 100)
+var data = make(map[int]string)
+var postChannel = make(chan string)
+var intChannel = make(chan int)
+var setChannel = make(chan string)
+var delChannel = make(chan int)
+
+func postData() {
+	for {
+		value := <-postChannel
+		data[len(data)+1] = value
+	}
+}
+
+func setData() {
+
+	for {
+		key := <-intChannel
+		value := <-setChannel
+		data[key] = value
+	}
+}
+
+func delData() {
+	for {
+		key := <-delChannel
+		data[key] = ""
+	}
+}
+
+func handler(conn net.Conn) {
+
+	defer conn.Close()
+
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		line := scanner.Text()
+		fields := strings.Fields(line)
+		if len(fields) < 1 {
+			continue
+		}
+
+		switch fields[0] {
+
+		case "POST":
+			var value string
+			if len(fields) < 2 {
+				io.WriteString(conn, "No value added! \n")
+			}
+			fieldArr := strings.Split(fields[1], "_")
+			if len(fieldArr) > 1 {
+				value = strings.Join(fieldArr, " ")
+			} else {
+				value = fields[1]
+			}
+			postChannel <- string(value)
+			io.WriteString(conn, value+" added!\n")
+		case "LIST":
+			for i := 0; i < len(data)+1; i++ {
+				if data[i] != "" {
+					io.WriteString(conn, fmt.Sprint(i)+": "+data[i]+"\n")
+				}
+			}
+		case "SET":
+			if len(fields) < 3 {
+				io.WriteString(conn, "Format should be <int Key> <string Value> \n")
+			}
+			var value string
+			keyInt, err := strconv.Atoi(fields[1])
+			if err != nil {
+				log.Fatal("Fatal error")
+			}
+			fieldArr := strings.Split(fields[2], "_")
+			if len(fieldArr) > 1 {
+				value = strings.Join(fieldArr, " ")
+			} else {
+				value = fields[2]
+			}
+			if data[keyInt] != "" {
+				io.WriteString(conn, "Key is already in use. Try the LIST command to see keys in use \n")
+			}
+			intChannel <- keyInt
+			setChannel <- value
+		case "DELETE":
+			keyInt, err := strconv.Atoi(fields[1])
+			if err != nil {
+				log.Fatal("Fatal error")
+			}
+			delChannel <- keyInt
+		default:
+			io.WriteString(conn, "Invalid Command "+fields[0]+"\n")
+		}
+
+	}
+
+}
+
+func startTCP() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer listener.Close()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+
+		go handler(conn)
+
+	}
+
+}
 
 func listData(w http.ResponseWriter, r *http.Request) {
 
-	for i := 1; i < len(dta)+1; i++ {
-		if dta[i] != "" {
+	for i := 1; i < len(data)+1; i++ {
+		if data[i] != "" {
 			fmt.Fprintf(w, strconv.FormatInt(int64(i), 10)+": ")
-			fmt.Fprintf(w, dta[i]+"\n")
+			fmt.Fprintf(w, data[i]+"\n")
 		}
 	}
 
@@ -35,11 +148,11 @@ func getData(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 	}
-	if dta[key] == "" {
+	if data[key] == "" {
 		fmt.Fprintf(w, fmt.Sprint(key)+" empty")
 	} else {
 		w.WriteHeader(http.StatusOK)
-		fmt.Fprintf(w, dta[key]+"\n")
+		fmt.Fprintf(w, data[key]+"\n")
 	}
 }
 
@@ -58,10 +171,9 @@ func post(w http.ResponseWriter, r *http.Request) {
 	} else {
 		value = parts[2]
 	}
-	dta[len(dta)+1] = string(value)
+	postChannel <- value
 }
 
-// Delete specified ID value
 func delete(w http.ResponseWriter, r *http.Request) {
 	parts := strings.Split(r.URL.String(), "/")
 
@@ -75,118 +187,26 @@ func delete(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, dta[key]+" deleted")
-	dta[key] = ""
-
-}
-
-//Handling the commands via TCP
-func handler(conn net.Conn) {
-
-	defer conn.Close()
-
-	scanner := bufio.NewScanner(conn)
-	for scanner.Scan() {
-		line := scanner.Text()
-		fields := strings.Fields(line)
-		if len(fields) < 1 {
-			continue
-		}
-
-		switch fields[0] {
-		case "GET":
-			if len(fields) < 2 {
-				io.WriteString(conn, "No key input! \n")
-			}
-			keyInt, err := strconv.Atoi(fields[1])
-			if err != nil {
-				log.Fatal("Fatal error")
-			}
-			io.WriteString(conn, dta[keyInt]+"\n")
-		case "SET":
-			if len(fields) < 3 {
-				io.WriteString(conn, "Format should be <int Key> <string Value> \n")
-			}
-			var value string
-			keyInt, err := strconv.Atoi(fields[1])
-			if err != nil {
-				log.Fatal("Fatal error")
-			}
-			fieldArr := strings.Split(fields[2], "_")
-			if len(fieldArr) > 1 {
-				value = strings.Join(fieldArr, " ")
-			} else {
-				value = fields[2]
-			}
-			if dta[keyInt] != "" {
-				io.WriteString(conn, "Key is already in use. Try the LIST command to see keys in use \n")
-			}
-			dta[keyInt] = value
-
-		case "POST":
-			var value string
-			if len(fields) < 2 {
-				io.WriteString(conn, "No value added! \n")
-			}
-			fieldArr := strings.Split(fields[1], "_")
-			if len(fieldArr) > 1 {
-				value = strings.Join(fieldArr, " ")
-			} else {
-				value = fields[1]
-			}
-			dta[len(dta)+1] = string(value)
-			io.WriteString(conn, value+" added!\n")
-		case "LIST":
-			for i := 0; i < len(dta)+1; i++ {
-				if dta[i] != "" {
-					io.WriteString(conn, fmt.Sprint(i)+": "+dta[i]+"\n")
-				}
-			}
-
-		case "DELETE":
-			keyInt, err := strconv.Atoi(fields[1])
-			if err != nil {
-				log.Fatal("Fatal error")
-			}
-			dta[keyInt] = ""
-		default:
-			io.WriteString(conn, "Invalid Command "+fields[0]+"\n")
-		}
-	}
-
-}
-
-//Starting the TCP connection
-func startTCP() {
-	listener, err := net.Listen("tcp", "localhost:8000")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer listener.Close()
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Print(err)
-			continue
-		}
-
-		handler(conn)
-
-	}
+	fmt.Fprintf(w, data[key]+" deleted")
+	delChannel <- key
 
 }
 
 func main() {
-	// Placeholder data for testing
-	dta[1] = "David"
-	dta[2] = "Ryan"
-	dta[3] = "Craig"
-	dta[4] = "Liam"
+
+	//Placeholder data for testing
+	data[1] = "Dan"
+	data[2] = "Sinead"
+
+	go postData()
+	go setData()
+	go delData()
+	go startTCP()
 
 	http.HandleFunc("/list", listData)
 	http.HandleFunc("/get/", getData)
 	http.HandleFunc("/post/", post)
 	http.HandleFunc("/delete/", delete)
-	go startTCP()
 	http.ListenAndServe(":8080", nil)
+
 }
